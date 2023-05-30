ea75a6255c0780b52044644ca6bee0801f866e13
behemoth-thainv <thainv@behemoth.vn>
2023-05-30 16:24:47 +0700

    test capture

---
+++ script.js
@@ -0,0 +1,12 @@
+var unloadCallback = (event) => {
+  event.preventDefault();
+  event.returnValue = "222";
+  return "111";
+};
+
+var confirmation = () => {
+  // alert("confirmation");
+};
+window.addEventListener("beforeunload", unloadCallback);
+window.addEventListener("popstate", confirmation());
+window.removeEventListener("beforeunload", unloadCallback);
